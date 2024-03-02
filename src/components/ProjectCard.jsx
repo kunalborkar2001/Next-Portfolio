c3833f90f7ffffc42065fcb42d84fede397894b1
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-03-02 14:25:57 +0530

    customized the card in projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,14 +12,14 @@ export default function ProjectCard() {
                     translateZ="50"
                     className="text-xl font-bold  text-white"
                 >
-                    Make things float in air
+                    Project Name
                 </CardItem>
                 <CardItem
                     as="p"
                     translateZ="60"
                     className=" text-sm max-w-sm mt-2 text-neutral-300"
                 >
-                    Hover over this card to unleash the power of CSS perspective
+                    Small description
                 </CardItem>
                 <CardItem translateZ="100" className="w-full mt-4">
                     <Image
@@ -34,16 +34,44 @@ export default function ProjectCard() {
                     <CardItem
                         translateZ={20}
                         as="button"
-                        className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                     >
-                        Try now →
+                        <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                            <span className="absolute inset-0 overflow-hidden rounded-full">
+                                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                            </span>
+                            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                                <span>
+                                    Watch Live
+                                </span>
+                                <svg
+                                    fill="none"
+                                    height="16"
+                                    viewBox="0 0 24 24"
+                                    width="16"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M10.75 8.75L14.25 12L10.75 15.25"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="1.5"
+                                    />
+                                </svg>
+                            </div>
+                            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+                        </button>
                     </CardItem>
                     <CardItem
                         translateZ={20}
                         as="button"
-                        className="px-4 py-2 rounded-xl  bg-white text-black  text-xs font-bold"
+
                     >
-                        Sign up
+
+                        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                            Shimmer
+                        </button>
+
                     </CardItem>
                 </div>
             </CardBody>
