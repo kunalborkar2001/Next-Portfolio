f7929f30bc98661394fac716a03f075989f9401e
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-04-03 14:01:22 +0530

    corrected path 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { MyContextProvider } from '../context/MyContext.js';
+import { MyContextProvider } from './context/MyContext';
 
 const inter = Inter({ subsets: ["latin"] });
 
