1b3d97680c633972995698e85c09c955ea75eb59
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-04-17 01:04:42 +0530

    added Skills section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import CardHoverEffectSkills from "../components/AboutMe"
 import Footer from "../components/Footer"
 import { useMyContext } from './context/MyContext.js'
 import { useEffect, useState } from 'react'
-
+import SkillsSection from '@/components/SkillsSection'
 
 export default function Home() {
 
@@ -46,18 +46,11 @@ export default function Home() {
     <div className='w-full relative'>
       <main>
         <HeroSection topTen={topTen} />
+        <SkillsSection />
         <CardHoverEffectSkills />
         <Footer />
       </main>
 
-      {/* <div className='absolute top-3 left-3'>
-        <div className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            Views &nbsp; <span className='text-[green]'>{2}</span>
-          </span>
-        </div>
-      </div> */}
     </div>
   );
 }
